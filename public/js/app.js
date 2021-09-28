45ed2ec6f4fd28fc2012f69fb1a866b58398e579
niranjana687 <niranjana687@gmail.com>
2021-09-28 20:37:25 +0530

    response error needs to be fixed

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,17 @@ const result = document.querySelector('#result');
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const search = query.value;
-    fetch("https://genius.p.rapidapi.com/search?q=Kendrick%20Lamar", {
+	console.log(search);
+    fetch("https://genius.p.rapidapi.com/search?q="+encodeURIComponent(search), {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "genius.p.rapidapi.com",
+		"x-rapidapi-key": "d84d3570cbmsh4cf2e7f5a3d5ce8p1e433ejsnde64c49690bc"
+	}
+})
+.then(response => {
+	const songID = response.response.hits[0].result.api_path;
+	fetch("https://genius.p.rapidapi.com/"+encodeURIComponent(songID), {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "genius.p.rapidapi.com",
@@ -16,6 +26,10 @@ searchForm.addEventListener('submit', (e) => {
 })
 .then(response => {
 	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
 })
 .catch(err => {
 	console.error(err);
